c226f92166e1e24c918926ff11ecfb74a1a62ae1
SAE1\1027502 <1027502@student.sae.edu.au>
2023-04-14 10:43:29 +1000

    Game Over scene for Knights crown

--- Assets/Ben's assets/Script/Exit.cs
+++ Assets/Ben's assets/Script/Exit.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+;
 
 public class Exit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public TextMeshProUGUI gameOverText;
+public GameObject triggerObject;
+
+public Text pointsText;
+
+
 
     // Update is called once per frame
     void Update()
