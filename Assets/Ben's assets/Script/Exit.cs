28a5537a5802a6a699518e3fc01c3c9036c1e281
LAPTOP-Q5C75GPS\sneep <warwickamy3@gmail.com>
2023-04-27 20:39:51 +1000

    assets

--- Assets/Ben's assets/Script/Exit.cs
+++ Assets/Ben's assets/Script/Exit.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
-;
 
 public class Exit : MonoBehaviour
 {
-public TextMeshProUGUI gameOverText;
-public GameObject triggerObject;
-
-public Text pointsText;
-
-
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
